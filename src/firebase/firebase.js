870e3a5aff0b71d8f7bd8a7ac6795b4f595f8b17
Aklima777 <aklimaa773@gmail.com>
2025-11-13 19:09:13 +0600

    firebase config updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPsl4gL_Vui2A6eOrqD0iZ-cSmvhNPZgE",
-  authDomain: "plateshare-89737.firebaseapp.com",
-  projectId: "plateshare-89737",
-  storageBucket: "plateshare-89737.firebasestorage.app",
-  messagingSenderId: "314348055851",
-  appId: "1:314348055851:web:8e96248cbc2cdcabdea96e"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
